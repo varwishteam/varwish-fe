ef2ca2b3a9bdf1f094be09cb7d133f9abb9c08b2
Tomáš Trejdl <tom.trejdl@seznam.cz>
2019-04-03 10:20:54 +0200

    Routes to a separate component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import Login from './pages/Login';
-import Home from './pages/Home';
 
 import theme from './themeConfig';
+import Routes from './components/Routes';
 
 /**
  * The base of the app, contains all first level routes
@@ -17,8 +15,7 @@ class App extends Component {
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
+          <Routes />
         </Router>
       </MuiThemeProvider>
     );
