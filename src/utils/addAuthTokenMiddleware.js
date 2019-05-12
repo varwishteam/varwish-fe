38e8b4c9597a774f52c7f29dbdca808099cc2370
Tomáš Trejdl <tom.trejdl@seznam.cz>
2019-05-12 16:48:12 +0200

    Fixed #22

--- src/utils/addAuthTokenMiddleware.js
+++ src/utils/addAuthTokenMiddleware.js
@@ -1,5 +1,5 @@
 import api from './api';
-import { USER } from '../actions';
+import { USER, logOut } from '../actions';
 
 export const addAuthTokenMiddleware = store => next => action => {
   if (action.type === USER.LOGIN.SUCCESS) api.setToken(action.payload.key);
@@ -9,6 +9,10 @@ export const addAuthTokenMiddleware = store => next => action => {
 
   // Remove token on logout
   if (action.type === USER.LOGOUT) api.setToken(null);
+
+  // Logout user when token becomes invalid
+  if (action.payload && action.payload.detail === 'Invalid token.')
+    store.dispatch(logOut());
 
   // continue processing this action
   return next(action);
