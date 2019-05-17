93260b18143497f3270d12f711576def26b0e11d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 21:03:07 +0000

    Deploy varwishteam/varwish-fe to github.com/varwishteam/varwish-fe.git:gh-pages

--- precache-manifest.05f4b5412f9d33bb36f9c23b27255622.js
+++ precache-manifest.05f4b5412f9d33bb36f9c23b27255622.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/varwish-fe/static/js/runtime~main.dde5b4bc.js"
   },
   {
-    "revision": "c65df919a656ba70c472",
-    "url": "/varwish-fe/static/js/main.642cbe62.chunk.js"
+    "revision": "1b0426ff7cba55961727",
+    "url": "/varwish-fe/static/js/main.baa141d2.chunk.js"
   },
   {
     "revision": "f0c5a19f287845055d62",
     "url": "/varwish-fe/static/js/2.9d9aecad.chunk.js"
   },
   {
-    "revision": "c65df919a656ba70c472",
+    "revision": "1b0426ff7cba55961727",
     "url": "/varwish-fe/static/css/main.9afadeea.chunk.css"
   },
   {
-    "revision": "1a0a722d2e8e8cc1b9597d67156dcb56",
+    "revision": "aa61d67ad26e2391a8dba65051feabb8",
     "url": "/varwish-fe/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/varwish-fe/precache-manifest.584ad1e30c0574502dfc02ab4ceebad1.js"
+  "/varwish-fe/precache-manifest.05f4b5412f9d33bb36f9c23b27255622.js"
 );
 
 workbox.clientsClaim();
